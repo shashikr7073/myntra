f2592f63d9613d0f91eba498fec9c11cd69fcec6
shashikr7073 <shashiroy4all@gmail.com>
2023-02-12 17:42:02 +0530

    First-Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+import { ProfileComponent } from './profile/profile.component';
+import { WomenComponent } from './women/women.component';
+import { MenComponent } from './men/men.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:MenComponent,
+  },
+  {
+    path:'women',
+    component:WomenComponent
+  },
+  {
+    path:'profile',
+    component:ProfileComponent
+    
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenComponent } from './men/men.component';
+import { WomenComponent } from './women/women.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenComponent,
+    WomenComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
